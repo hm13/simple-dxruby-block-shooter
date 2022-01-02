bb4e3b938c4580abaf1112ce1f7872e8006adf11
hm13 <thirteeeeeeeeeeeeen@gmail.com>
2022-01-03 01:16:02 +0900

    add blocks

--- main.rb
+++ main.rb
@@ -1,29 +1,18 @@
 require 'dxruby'
 require 'json'
 
-class SpriteJsonInit < Sprite
-    attr_accessor :vx, :vy
-    protected :vx, :vy
-
-    def initialize
-        all_config = File.open("config.json") do |j|
-            JSON.load(j)
-        end
-
-        config = all_config[self.class.to_s]
-
-        self.x = config["x"]
-        self.y = config["y"]
-        self.vx = config["vx"]
-        self.vy = config["vy"]
-        self.image = Image.new(config["w"],config["h"],eval(config["c"]))
+class Ball < Sprite
+    def initialize(x,y,image)
+        super(x,y,image)
+        @vx = 5
+        @vy = 5
+        @sign_x = 1
+        @sign_y = 1
     end
-end
 
-class Ball < SpriteJsonInit
     def update
-        self.x += self.vx
-        self.y += self.vy
+        self.x += @sign_x * @vx
+        self.y += @sign_y * @vy
     end
 
     def hit(o)
@@ -33,47 +22,97 @@ class Ball < SpriteJsonInit
 
         # Estimate direction the Ball(self) shot the object(o).
         # Inaccruate, but simple algorithm.
-        is_horizontal_shot = (o.image.width - self.center_x.abs < 0)
+        is_horizontal_shot = ((o.x + o.center_x) - (self.x + self.center_x)).abs > (o.image.width / 2)
 
         if is_horizontal_shot
-            self.vx = -self.vx
+            @sign_x = -@sign_x
         else
-            self.vy = -self.vy
+            @sign_y = -@sign_y
+        end
+
+        if o.instance_of?(Bar)
+            # Calculate how far Ball(self) off from the center of the Bar(o) (in -0.5 ~ 0.5).
+            pos = ((self.x + self.center_x) - (o.x + o.center_x)).to_f / o.image.width
+
+            # Invert the result depending on @sign_x
+            pos = @sign_x * pos
+
+            if pos < -0.4
+                @vx = 1
+                @vy = 6
+            elsif pos < -0.3
+                @vx = 3
+                @vy = 5
+            elsif pos < 0.3
+                @vx = 5
+                @vy = 5
+            elsif pos < 0.4
+                @vx = 5
+                @vy = 3
+            else
+                @vx = 6
+                @vy = 1
+            end
         end
     end
 end
 
-class Bar < SpriteJsonInit
-    def update
-        self.x += Input.x * self.vx
+class Block < Sprite
+    def initialize(x, y,image)
+        super(x,y,image)
+        self.x = x
+        self.y = y
+    end
+
+    def hit(o)
+        self.vanish
     end
 end
 
-class WallTop < SpriteJsonInit
+class Bar < Sprite
+    def initialize(x,y,image)
+        super(x,y,image)
+        @vx = 7
+    end
+
+    def update
+        self.x += Input.x * @vx
+    end
 end
 
-class WallBottom < SpriteJsonInit
+class WallTop < Sprite
+end
+
+class WallBottom < Sprite
     def hit(o)
         if o.instance_of?(Ball)
-            o.vanish
+            # o.vanish
         end
     end
 end
 
-class WallLeft < SpriteJsonInit
+class WallLeft < Sprite
 end
 
-class WallRight < SpriteJsonInit
+class WallRight < Sprite
 end
 
-objects = [
-    Bar.new,
-    Ball.new,
-    WallTop.new,
-    WallBottom.new,
-    WallLeft.new,
-    WallRight.new
-]
+
+all_config = File.open("config.json") do |j|
+    JSON.load(j)
+end
+
+objects = []
+
+all_config.each do |(class_name,object_param_list)|
+    object_param_list.each do |c|
+        x = c["x"]
+        y = c["y"]
+        image = Image.new(c["w"],c["h"],eval(c["c"]))
+    
+        objects << eval(class_name).new(x,y,image)
+    end
+end
 
 Window.loop do
     Sprite.update(objects)
