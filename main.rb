0b9db9c6a45e9a5967be904e601ccd428378a61d
hm13 <thirteeeeeeeeeeeeen@gmail.com>
2022-01-04 23:06:07 +0900

    add game status (start/playing/result)

--- main.rb
+++ main.rb
@@ -89,7 +89,7 @@ end
 class WallBottom < Sprite
   def hit(o)
     if o.instance_of?(Ball)
-      # o.vanish
+      o.vanish
     end
   end
 end
@@ -100,24 +100,72 @@ end
 class WallRight < Sprite
 end
 
-all_config = File.open("config.json") do |j|
-  JSON.load(j)
-end
+class Game
+  DisplayFont = Font.new(32)
 
-objects = []
+  module Status
+    START = 1
+    PLAYING = 2
+    RESULT = 3
+  end
 
-all_config.each do |(class_name, object_param_list)|
-  object_param_list.each do |c|
-    x = c["x"]
-    y = c["y"]
-    image = Image.new(c["w"], c["h"], eval(c["c"]))
+  def initialize
+    @objects = []
+    @status = Status::START
+    @result = ""
+  end
 
-    objects << eval(class_name).new(x, y, image)
+  def load
+    @objects = []
+
+    all_config = File.open("config.json") do |j|
+      JSON.load(j)
+    end
+
+    all_config.each do |(class_name, object_param_list)|
+      object_param_list.each do |c|
+        x = c["x"]
+        y = c["y"]
+        image = Image.new(c["w"], c["h"], eval(c["c"]))
+
+        @objects << eval(class_name).new(x, y, image)
+      end
+    end
+  end
+
+  def run
+    Window.loop do
+      case @status
+      when Status::START
+        Window.draw_font(0, 0, "Press SPACE to start.", DisplayFont)
+
+        if Input.key_push?(K_SPACE)
+          self.load
+          @status = Status::PLAYING
+        end
+      when Status::PLAYING
+        Sprite.update(@objects)
+        Sprite.draw(@objects)
+        Sprite.check(@objects)
+        Sprite.clean(@objects)
+
+        if @objects.find_all { |o| o.instance_of?(Ball) } == []
+          @status = Status::RESULT
+          @result = "FAILD!"
+        elsif @objects.find_all { |o| o.instance_of?(Block) } == []
+          @status = Status::RESULT
+          @result = "CLEAR!"
+        end
+      when Status::RESULT
+        Window.draw_font(0, 0, @result + " (Press SPACE to try again.)", DisplayFont)
+
+        if Input.key_push?(K_SPACE)
+          self.load
+          @status = Status::PLAYING
+        end
+      end
+    end
   end
 end
 
-Window.loop do
-  Sprite.update(objects)
-  Sprite.draw(objects)
-  Sprite.check(objects)
-end
+Game.new.run
